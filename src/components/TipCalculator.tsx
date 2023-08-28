f6c147879c2391e09716e35baa9567ade1b61ea9
Yan Fernandes <yanfernandes404@gmail.com>
2023-08-27 23:45:33 -0300

    feat: implement react-redux hooks

--- src/components/TipCalculator.tsx
+++ src/components/TipCalculator.tsx
@@ -1,29 +1,66 @@
 import { useSelector } from "react-redux";
 import { StoreState } from "../store";
+import { ActionTypes } from "../store/actions";
+import { useDispatch } from "react-redux";
 
 function TipCalculator() {
   const bill = useSelector((state: StoreState) => state.bill);
-  const tip = useSelector((state: StoreState) => state.tip);
   const split = useSelector((state: StoreState) => state.split);
   const percentage = useSelector((state: StoreState) => state.percentage);
+
+  const dispatch = useDispatch();
 
   return (
     <div>
       <div>
         <span>Bill: </span>
-        <input type="text" value={bill} />
+        <input
+          type="text"
+          value={bill}
+          onChange={(e) =>
+            dispatch({
+              type: ActionTypes.BillChange,
+              payload: e.target.value,
+            })
+          }
+        />
       </div>
 
       <div>
         <span>Tip %: </span>
-        <input type="text" value={percentage} />
+        <input
+          type="text"
+          value={percentage}
+          onChange={(e) =>
+            dispatch({
+              type: ActionTypes.PercentageChange,
+              payload: e.target.value,
+            })
+          }
+        />
       </div>
 
       <div>
         <span>Split: </span>
-        <button>+</button>
-        <span>{1}</span>
-        <button>-</button>
+        <button
+          onClick={() =>
+            dispatch({
+              type: ActionTypes.SplitIncrement,
+            })
+          }
+        >
+          +
+        </button>
+        <span>{split}</span>
+        <button
+          onClick={() =>
+            dispatch({
+              type: ActionTypes.SplitDecrement,
+            })
+          }
+        >
+          -
+        </button>
       </div>
     </div>
   );
